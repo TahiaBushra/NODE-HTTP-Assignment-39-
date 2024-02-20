f2224921386ff8eed57544bec8037c10b9cf64be
TahiaBushra <bushratahia@gmail.com>
2024-02-20 17:40:19 +0600

    http surver done

--- app.js
+++ app.js
@@ -11,20 +11,51 @@
 //   console.log(`Server is running on port: ${PORT}`);
 // });
 
+// const http = require("http");
+
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { "Content-Type": "application/json" });
+
+//   const jsonRes = {
+//     message: "Assalamualaikum! How are you today ?",
+//     timestamp: new Date().toLocaleDateString(),
+//   };
+//   res.end(JSON.stringify(jsonRes));
+// });
+
+// const PORT = 3000;
+
+// server.listen(PORT, () => {
+//   console.log(`Server is running on port: ${PORT}`);
+// });
+
+//
+
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
+  const url = req.url;
 
-  const jsonRes = {
-    message: "Assalamualaikum! How are you today ?",
-    timestamp: new Date().toLocaleDateString(),
-  };
-  res.end(JSON.stringify(jsonRes));
+  switch (url) {
+    case "/":
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end("Welcome to home");
+      break;
+    case "/about":
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end("Welcome to about");
+      break;
+    case "/contact":
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end("Welcome to contact");
+      break;
+    default:
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end("Page not found");
+  }
 });
 
-const PORT = 3000;
-
+const PORT = 8080;
 server.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
